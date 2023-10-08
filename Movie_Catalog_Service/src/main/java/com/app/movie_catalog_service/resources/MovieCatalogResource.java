43e00bf278d28f0e77e1bfee736c99052d505588
oussamaAdaoumoum <oussamaadm755@gmail.com>
2023-10-08 02:20:27 +0100

    connect the catalog with the two other services synchronously

--- Movie_Catalog_Service/src/main/java/com/app/movie_catalog_service/resources/MovieCatalogResource.java
+++ Movie_Catalog_Service/src/main/java/com/app/movie_catalog_service/resources/MovieCatalogResource.java
@@ -1,21 +1,60 @@
 package com.app.movie_catalog_service.resources;
 
 import com.app.movie_catalog_service.models.CatalogItem;
+import com.app.movie_catalog_service.models.Movie;
+import com.app.movie_catalog_service.models.Rating;
+import com.app.movie_catalog_service.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    // here we used a java bean, to not create a restTemplate every time we make a rest call to catalog
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /*
+    *what we want to do
+    * get all the movie IDs
+    * for each movie ID, call movie info service and get details
+    * put them all together
+     */
+
+    /*
+    *now we just gonna hardcode the rating list part
+     */
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Nice Film", 4)
-        );
+      //  webClient.Builder: you can use it like restTemplate but this one is asynchronous
+
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsData/users/"+ userId, UserRating.class);
+        return ratings.getUserRating().stream().map(rating ->{
+            // this is a synchronous call, you can make it asynchronous if you use web client
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieID(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+    //    return Collections.singletonList(
+    //            new CatalogItem("Transformers", "Nice Film", 4)
+    //    );
     }
 }
+
+/*
+* webClientBuilder.build() // the asynchrounous method
+* .get() // get what is in the uri
+* .uri("http://localhost:8082/movies/" +rating.getMovieId()) // the uri where the service is presents
+* .retrieve() // et the data from the service
+* .bodyToMono() // to say wait for the data
+* .block(); // make the code synchrounous "block the code untile getting the data"
+* */
--- Ratings_Data_Service/src/main/java/com/app/ratings_data_service/resources/RatingsResource.java
+++ Ratings_Data_Service/src/main/java/com/app/ratings_data_service/resources/RatingsResource.java
@@ -2,9 +2,13 @@ package com.app.ratings_data_service.resources;
 
 
 import com.app.ratings_data_service.models.Rating;
+import com.app.ratings_data_service.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsData")
@@ -13,6 +17,18 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRatings(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId){
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2345", 8)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 
 }
